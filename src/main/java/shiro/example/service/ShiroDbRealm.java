294d1b7d8767891209e4771e836e1e3ecc2e619a
unknown <kaer.lke@ali-80860n.hz.ali.com>
2014-10-11 02:39:44 +0800

    add description

--- src/main/java/shiro/example/service/ShiroDbRealm.java
+++ src/main/java/shiro/example/service/ShiroDbRealm.java
@@ -19,8 +19,12 @@ public class ShiroDbRealm extends AuthorizingRealm {
 
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
-        UsernamePasswordToken loginToken = (UsernamePasswordToken)authenticationToken;
-        System.out.println("Username : " + loginToken.getUsername());
-        return new SimpleAuthenticationInfo(loginToken, "123456" , loginToken.getPassword().toString());
+        //表单信息存储在authenticationToken,对于默认的用户密码采用强转内置的UsernamePasswordToken.
+        //对于特殊的用户名密码，需要自建相应的token，因此可见俊杰为了兼容域名增加了一个MyAuthenticationToken
+        UsernamePasswordToken formData = (UsernamePasswordToken)authenticationToken;
+        //
+        System.out.println("Username : " + formData.getUsername());
+        //
+        return new SimpleAuthenticationInfo(formData, "123456" , "AnyThing else");
     }
 }
