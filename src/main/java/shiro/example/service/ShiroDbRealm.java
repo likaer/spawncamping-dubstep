9edb683dd8ba10e5c97e65f8cd179f17b0a1959e
unknown <kaer.lke@ali-80860n.hz.ali.com>
2014-10-23 23:16:39 +0800

    添加用户权限认证

--- src/main/java/shiro/example/service/ShiroDbRealm.java
+++ src/main/java/shiro/example/service/ShiroDbRealm.java
@@ -4,17 +4,30 @@ package shiro.example.service;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.subject.Subject;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ShiroDbRealm extends AuthorizingRealm {
 
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
-        return null;
+        final String username = (String) principalCollection.getPrimaryPrincipal();
+        //final Set<String> roles = this.emailToRoles.get(username);
+        //return new SimpleAuthorizationInfo(roles);
+        final Set<String> roles = new HashSet<String>(){
+            {
+                this.add("admin");
+            }
+        };
+        //TODO roleService.findRolesByUsername();
+        //TODO addRoles to Set<String>roles
+        return new SimpleAuthorizationInfo(roles);
     }
 
     @Override
@@ -24,7 +37,11 @@ public class ShiroDbRealm extends AuthorizingRealm {
         UsernamePasswordToken formData = (UsernamePasswordToken)authenticationToken;
         //
         System.out.println("Username : " + formData.getUsername());
-        //
-        return new SimpleAuthenticationInfo(formData, "123456" , "AnyThing else");
+        String password = "123456";
+        //从数据库获取该用户的密码。
+        //password = UserService.getUserByUserName(formData.getUsername()).getPassword();
+        //Shiro会调用内部方法去匹配formData中的密码是否等于password参数
+        //其中"AnyThing else"是凭证名，一般填用户名，但是并无明确校验！
+        return new SimpleAuthenticationInfo(formData, password, "AnyThing else");
     }
 }
